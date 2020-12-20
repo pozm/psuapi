19b6885750718d4e01c7ab864e31b0ab7ae9ff5f
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2020-12-20 22:56:39 +0100

    Remove useless stuff and change let variables that dont change to const
    also made the settings not autistic

--- index.js
+++ index.js
@@ -1,67 +1,49 @@
-const { exec } = require("child_process");
-let fs = require("fs")
-let fetch = require("node-fetch")
-async function obfuscate(key,script,options){
-    let body = {
+const fetch = require("node-fetch");
+
+async function obfuscate(key, script, options){
+    const body = {
         "key": key,
         "script": script,
         "options": {
-            "DisableSuperOperators": false,
-            "MaximumSecurityEnabled": false,
-            "ControlFlowObfuscation": true,
-            "ConstantEncryption": false,
-            "EncryptAllStrings": false,
-            "DisableAllMacros": false,
-            "EnhancedOutput": false,
-            "EnhancedConstantEncryption": false,
-            "CompressedOutput": false,
-            "PremiumFormat": false,
-            "ByteCodeMode": "Default"
+            "DisableSuperOperators": options.DisableSuperOperators || false,
+            "MaximumSecurityEnabled": options.MaximumSecurityEnabled || false,
+            "ControlFlowObfuscation": options.ControlFlowObfuscation || true,
+            "ConstantEncryption": options.ConstantEncryption || false,
+            "EncryptAllStrings": options.EncryptAllStrings || false,
+            "DisableAllMacros": options.DisableAllMacros || false,
+            "EnhancedOutput": options.EnhancedOutput || false,
+            "EnhancedConstantEncryption": options.EnhancedConstantEncryption || false,
+            "CompressedOutput": options.CompressedOutput || false,
+            "PremiumFormat": options.PremiumFormat || false,
+            "ByteCodeMode": options.ByteCodeMode || "Default"
         }
     }
-    let possibleops = [
-        "DisableSuperOperators",
-        "MaximumSecurityEnabled",
-        "ControlFlowObfuscation",
-        "ConstantEncryption",
-        "EncryptAllStrings",
-        "DisableAllMacros",
-        "EnhancedOutput",
-        "EnhancedConstantEncryption",
-        "CompressedOutput",
-        "PremiumFormat",
-        "ByteCodeMode"
-    ]
-    for(let i = 0;i < possibleops.length;i++){
-        if(options[possibleops[i]] != undefined || options[possibleops[i]] != "undefined"){
-            body.options[possibleops[i]] = options[possibleops[i]]
-        }
-    }
-    let a = await fetch('http://api.psu.dev:8080/obfuscate',{
+
+    const a = await fetch('http://api.psu.dev:8080/obfuscate',{
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    let info = {
+    const info = {
         "script": await a.text(),
         "reqsleft": a.headers.get('KeyUsed'),
-        "keyused": a.headers.get('ReqsLeft'),
+        "keyused": a.headers.get('ReqsLeft')
     }
     return info;
 }
 
 async function checkkey(key){
-    let a = await fetch('http://api.psu.dev:8080/checkkey',{
+    const a = await fetch('http://api.psu.dev:8080/checkkey',{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'key': key
         }
     })
-    let a_json = await a.json();
-    return a_json
+    const json = await a.json();
+    return json;
 }
 
-module.exports = {obfuscate,checkkey};
+module.exports = {obfuscate: obfuscate, checkKey: checkkey}
